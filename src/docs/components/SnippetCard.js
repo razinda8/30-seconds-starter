484ae5bc21cdd0438783bc515fdbc4ad89dea800
Angelos Chalaris <chalarangelo@gmail.com>
2019-07-18 14:35:44 +0300

    Optimize nodes in snippet cards

--- src/docs/components/SnippetCard.js
+++ src/docs/components/SnippetCard.js
@@ -36,6 +36,43 @@ const getCodeBlocks = str => {
   };
 };
 
+// Optimizes nodes in an HTML string
+const optimizeNodes = (data, regexp, replacer) => {
+  let count = 0;
+  let output = data;
+  do {
+    output = output.replace(regexp, replacer);
+    count = 0;
+    while (regexp.exec(output) !== null)
+      ++count;
+
+  } while (count > 0);
+  return output;
+};
+
+const optimizeAllNodes = (html) => {
+  let output = html;
+  // Optimize punctuation nodes
+  output = optimizeNodes(
+    output,
+    /<span class="token punctuation">([^\0<]*?)<\/span>([\n\r\s]*)<span class="token punctuation">([^\0]*?)<\/span>/gm,
+    (match, p1, p2, p3) => `<span class="token punctuation">${p1}${p2}${p3}</span>`
+  );
+  // Optimize operator nodes
+  output = optimizeNodes(
+    output,
+    /<span class="token operator">([^\0<]*?)<\/span>([\n\r\s]*)<span class="token operator">([^\0]*?)<\/span>/gm,
+    (match, p1, p2, p3) => `<span class="token operator">${p1}${p2}${p3}</span>`
+  );
+  // Optimize keyword nodes
+  output = optimizeNodes(
+    output,
+    /<span class="token keyword">([^\0<]*?)<\/span>([\n\r\s]*)<span class="token keyword">([^\0]*?)<\/span>/gm,
+    (match, p1, p2, p3) => `<span class="token keyword">${p1}${p2}${p3}</span>`
+  );
+  return output;
+}
+
 const CardCorner = ({ difficulty = 'intermediate' }) => (
   <div className={`corner ${difficulty}`} aria-label={difficulty} title={difficulty} />
 );
@@ -47,8 +84,8 @@ const SnippetCard = ({snippetData}) => {
     <h4 id=${snippetData.title}>${snippetData.title}</h4>
     ${getTextualContent(snippetData.html)}
   `;
-  let cardCodeHtml = `${getCodeBlocks(snippetData.html).code}`;
-  let cardExamplesHtml = `${getCodeBlocks(snippetData.html).example}`;
+  let cardCodeHtml = `${optimizeAllNodes(getCodeBlocks(snippetData.html).code)}`;
+  let cardExamplesHtml = `${optimizeAllNodes(getCodeBlocks(snippetData.html).example)}`;
   let difficulty = snippetData.tags.includes('advanced') ? 'advanced' : snippetData.tags.includes('beginner') ? 'beginner' : 'intermediate';
 
   return ( 
