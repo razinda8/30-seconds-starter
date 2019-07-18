5921e421b601f786b8659617155f247db952ceb4
Angelos Chalaris <chalarangelo@gmail.com>
2019-07-18 14:18:19 +0300

    Add category listing in front page

--- src/docs/pages/index.js
+++ src/docs/pages/index.js
@@ -26,9 +26,9 @@ const IndexPage = (props) => {
       <Meta />
       <Shell>
         <h1 className='landing-title'>{site.title}<small>{site.description}</small></h1>
-        <input type='search' className='body-search' placeholder='Search for snippets'></input>
+        {/* <input type='search' className='body-search' placeholder='Search for snippets'></input> */}
         <h4>Snippet categories</h4>
-        <ul>
+        <ul className='category-list card'>
           { tags.map(tag => (
             <li key={tag}><Link to={`/tags/${tag}`}>{capitalize(tag)}</Link></li>
           ))}
